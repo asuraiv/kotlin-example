ee988870d7a6390d0f506663fd75db2383211e36
asuraiv <asuraiv7@gmail.com>
2020-06-08 22:35:14 +0900

    with, apply 관련 예제코드 추가

---
+++ src/main/kotlin/com/asuraiv/kotlinpactice/applyandwith/WithTest.kt
@@ -0,0 +1,63 @@
+package com.asuraiv.kotlinpactice.applyandwith
+
+import java.lang.StringBuilder
+
+class WithTest {
+
+    companion object {
+
+        @JvmStatic
+        fun main(args: Array<String>) {
+
+            val alphabet = fun() : String {
+
+                val result = StringBuilder()
+
+                for(letter in 'A'..'Z') {
+                    result.append(letter)
+                }
+
+                result.append("\nNow I know the alphabet!")
+
+                return result.toString()
+            }
+
+            println(alphabet())
+
+
+            val withAlphabet = fun() : String {
+
+                val stringBuilder = StringBuilder()
+
+                return with(stringBuilder) {// 첫번째 파라미터는 stringbuilder, 두번째는 람다
+
+                    for(letter in 'A'..'Z') {
+
+                        this.append(letter) // this 사용
+                    }
+
+                    append("\nNow I know the alphabet!") // this 생략 가능
+
+                    this.toString()
+                }
+            }
+
+            println(withAlphabet())
+
+            // 좀 더 깔끔한 방법
+            fun refactorWithAlphabet() = with(StringBuilder()) {
+
+                for(letter in 'A'..'Z') {
+
+                    append(letter) // this 사용
+                }
+
+                append("\nNow I know the alphabet!") // this 생략 가능
+
+                toString()
+            }
+
+            println(refactorWithAlphabet())
+        }
+    }
+}
